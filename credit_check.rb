f30d0f9ca18321df6b11cfdf203b7fe101a2f128
Jason Wright <noobjey@gmail.com>
2015-05-30 16:11:51 -0600

    before refactoring to test methods in isolation

--- credit_check.rb
+++ credit_check.rb
@@ -1,13 +1,23 @@
 class CreditCheck
   def validate(number)
-    result = []
+    mutiplied_by_two = []
     number.each_with_index do |num, index|
       if index.even?
-        result << num
+        mutiplied_by_two << num
       else
-        result << num * 2
+        mutiplied_by_two << num * 2
       end
     end
-    result
+
+    sum_digits_over_10 = mutiplied_by_two.map do |num|
+      if num > 9
+        puts "num [#{num}]"
+        puts "n1 + n2 *********************************[#{num.to_s[0].to_i + num.to_s[1].to_i}]"
+        (num.to_s[0].to_i + num.to_s[1].to_i)
+      else
+        num
+      end
+    end
+    sum_digits_over_10
   end
 end
